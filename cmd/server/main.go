89cbb45323d1528dec516c241c14e99a68d48a53
adiletelf <adiletelf@gmail.com>
2022-06-15 22:00:15 +0600

    Make controllers reusable by exposing them

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/adiletelf/payment-system-go/pkg/api"
-	// "github.com/adiletelf/payment-system-go/pkg/models"
+	"github.com/gin-gonic/gin"
 )
 
+
+func handleRequests(r *gin.Engine) {
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+	r.GET("/transaction", api.ReturnTransaction)
+}
 
 func main() {
 	// db, err := api.SetupDB()
@@ -12,6 +23,7 @@ func main() {
 	// 	log.Fatalf(err.Error())
 	// }
 
-	r := api.GetRouter()
+	r := gin.Default()
+	handleRequests(r)
 	r.Run(":8080")
 }
--- pkg/api/controllers.go
+++ pkg/api/controllers.go
@@ -7,18 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "ok",
-		})
-	})
-	r.GET("/transaction", returnTransaction)
-
-	return r
-}
-
-func returnTransaction(c *gin.Context) {
+func ReturnTransaction(c *gin.Context) {
 	c.JSON(http.StatusOK, models.NewTransaction(models.User{ID: 1, Email: "test@gmail.com"}, 99.99, models.Ruble))
 }
